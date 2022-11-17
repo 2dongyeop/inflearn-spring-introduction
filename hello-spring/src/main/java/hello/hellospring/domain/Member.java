bce3ea1133ffbca1298164cf66f8ffacb78b359b
leedongyeop <ldy_1204@naver.com>
2022-11-17 15:23:00 +0900

    JPA 엔티티 매핑

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 알아서 생성해줌
     private Long id;
+
     private String name;
 
     public String getName() {
